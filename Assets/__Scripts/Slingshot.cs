8df9ee2adb77f2879216c1d017bbc0cc999c0eb4
peterduong968 <54965701+peterduong968@users.noreply.github.com>
2019-09-30 23:43:11 -0400

    Step 2. Able to shoot projectile

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -9,10 +9,14 @@ public class Slingshot : MonoBehaviour {
 
     // Place class variables here
     public GameObject prefabProjectile;
+    public float velocityMult = 8f;
+
+    [Header("Set Dynamically")]
     public GameObject launchPoint;
     public Vector3 launchPos;
     public GameObject projectile;
     public bool aimingMode;
+    private Rigidbody projectileRigidbody;
 
     void Awake()
     {
@@ -45,5 +49,45 @@ public class Slingshot : MonoBehaviour {
         //set it to isKinematic for now
         projectile.GetComponent<Rigidbody>().isKinematic = true;
 
+        //set it to isKinematic for now
+        projectileRigidbody = projectile.GetComponent<Rigidbody>();
+        projectileRigidbody.isKinematic = true;
     }
+
+    void Update()
+    {
+        //if slingshot is not in aimingMode, don't run
+        if (!aimingMode)
+            return;
+
+        //get the current mouse position in 2d screen coordinates
+        Vector3 mousePos2d = Input.mousePosition;
+        mousePos2d.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2d);
+
+        //Find the delta from the launchPos to the mousePos3D
+        Vector3 mouseDelta = mousePos3D - launchPos;
+
+        //Limit mouseDelta to the radius of the slingshot SphereCollider
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if (mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        //move the projectile to this new position
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        if (Input.GetMouseButton(0))
+        {
+            //the mouse has been released
+            aimingMode = false;
+            projectileRigidbody.isKinematic = false;
+            projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            projectile = null;
+        }
+    }
+
+
 }
