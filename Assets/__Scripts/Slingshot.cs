14ef7b726591ba91d0f7b2dcf27be45ac1fc06e6
peterduong968 <54965701+peterduong968@users.noreply.github.com>
2019-09-30 23:22:12 -0400

    Step 1. Projectile Appears

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -5,22 +5,45 @@ using UnityEngine;
 // YOU must implement the Slingshot
 
 public class Slingshot : MonoBehaviour {
-  
+    [Header("Set in Inspector")]
 
-// Place class variables here
+    // Place class variables here
+    public GameObject prefabProjectile;
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
 
-
-
-    private void Awake()
+    void Awake()
     {
-
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
     }
 
-
-    private void Update()
+    void OnMouseEnter()
     {
- 
+        //print("Slingshot: OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
 
+    void OnMouseExit()
+    {
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    private void OnMouseDown()
+    {
+        //player has pressed the  mouse button
+        aimingMode = true;
+        //instantiate a projectile
+        projectile = Instantiate( prefabProjectile ) as GameObject;
+        //start it at launchpoint
+        projectile.transform.position = launchPos;
+        //set it to isKinematic for now
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
 
     }
 }
